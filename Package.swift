d8aae0ecf03f1a27d535612c8796c095a1b51a00
Magela Santana <magela.santana@lyra-network.com>
2026-01-29 11:49:42 +0100

    add Package file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "LyraMotion",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "LyraMotion",
+            targets: ["LyraMotion"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "LyraMotion",
+            url: "https://raw.githubusercontent.com/MageSantana/Motionl/1.0.9/LyraMotion.xcframework.zip",
+            checksum: "a6230ff0a0e8b596c6bad44a51c62af5db0dc9565fe6239e35545df2027645bf"
+        )
+    ]
+)
